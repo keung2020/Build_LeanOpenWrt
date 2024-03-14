27af5a5cfddbbd212baaecec0602d813fe5b48b3
keung2020 <62239958+keung2020@users.noreply.github.com>
2024-03-14 09:53:54 +0800

    Update feed-clean.sh

--- feed-clean.sh
+++ feed-clean.sh
@@ -2,6 +2,30 @@
 
 # Description: OpenWrt DIY script run after Update feeds
 
+function merge_package() {
+    # 参数1是分支名,参数2是库地址,参数3是所有文件下载到指定路径。
+    # 同一个仓库下载多个文件夹直接在后面跟文件名或路径，空格分开。
+    if [[ $# -lt 3 ]]; then
+        echo "Syntax error: [$#] [$*]" >&2
+        return 1
+    fi
+    trap 'rm -rf "$tmpdir"' EXIT
+    branch="$1" curl="$2" target_dir="$3" && shift 3
+    rootdir="$PWD"
+    localdir="$target_dir"
+    [ -d "$localdir" ] || mkdir -p "$localdir"
+    tmpdir="$(mktemp -d)" || exit 1
+    git clone -b "$branch" --depth 1 --filter=blob:none --sparse "$curl" "$tmpdir"
+    cd "$tmpdir"
+    git sparse-checkout init --cone
+    git sparse-checkout set "$@"
+    # 使用循环逐个移动文件夹
+    for folder in "$@"; do
+        mv -f "$folder" "$rootdir/$localdir"
+    done
+    cd "$rootdir"
+}
+
 #Delete duplicate file
 #rm -rf feeds/luci/applications/luci-app-aliddns
 #rm -rf feeds/luci/applications/luci-app-autoreboot
@@ -52,4 +76,6 @@
 #rm -rf feeds/packages/net/smartdns
 #rm -rf feeds/packages/net/shadowsocks-rust
 rm -rf feeds/small/gn
+rm -rf feeds/small/shadowsocks-rust
+merge_package openwrt-23.05 https://github.com/immortalwrt/packages feeds/small net/shadowsocks-rust
 ./scripts/feeds update -a
